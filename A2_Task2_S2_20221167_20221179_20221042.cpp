58f3ef22b487cc4598e56e6246c5291b42c24977
Menna Essam <113799094+Menna242@users.noreply.github.com>
2023-11-03 04:26:13 +0200

    Update A2_Task2_S2_20221167_20221179_20221042.cpp

--- A2_Task2_S2_20221167_20221179_20221042.cpp
+++ A2_Task2_S2_20221167_20221179_20221042.cpp
@@ -1,10 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
-#include "A2_Task2_S2.h"
+#include "A2_Task2.h"
+//#include "Task2.cpp"
+
 int main(){
+    cout<<"Enter the BigNumber:";
     string s;
     getline(cin,s);
     BigReal o1;
     BigReal oo(s);
-    oo.printf();
+      oo.printf();
+
+    string ss;
+    getline(cin,ss);
+    BigReal oo2(ss);
+    oo2.printf();
+
+   oo.operator==(oo2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
 }
