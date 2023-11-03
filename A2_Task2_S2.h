f325337cb85cadf0c2a187e3b8b123257eca31bc
Menna Essam <113799094+Menna242@users.noreply.github.com>
2023-11-03 04:25:45 +0200

    Update A2_Task2_S2.h

--- A2_Task2_S2.h
+++ A2_Task2_S2.h
@@ -1,8 +1,7 @@
-//#include <bits/stdc++.h>
-//using namespace std;
 class BigReal{
 private:
     string BigNumber,integer,fraction;
+   int intSize,fractionSize ;
     char sign;
 public:
 
@@ -12,6 +11,16 @@ public:
     }
 
     BigReal(string real){
+        if(real[0]=='+'||real[0]=='-') {
+            for (int i = 1; i < real.size(); ++i) {
+                if(!isdigit(real[i])&&real[i]!='.'){
+                    real[i]='0';
+                }
+                else{
+                    break;
+                }
+            }
+        }
         if(regex_match(real,regex("[+-]?\\d+"))){
             if(real[0]=='+'){  //+11
                 sign='+';
@@ -33,7 +42,7 @@ public:
                 fraction="0";
             }
         }
-       else if(regex_match(real,regex("[+-]?\\d*.?\\d+"))){
+        else if(regex_match(real,regex("[+-]?\\d*.?\\d+"))){
             if(real[0]=='+'){  //+11.11
                 sign='+';
                 real[0]='0';
@@ -65,16 +74,97 @@ public:
             }
         }
 
-
         else{
-            BigNumber="0.0";
+            cout<<" the big number is invalid.\n";
+            integer="0";
+            fraction="0";
+        }
+
+         intSize=integer.size(); fractionSize=fraction.size();
+         BigNumber=sign+integer+'.'+fraction;
+
+    }
+//----------------------------------------------------------------------------------
+    bool Size1(BigReal number2){
+        BigReal result,result2;
+        if(intSize!=number2.intSize){
+            if(number2.intSize<intSize){
+                for (int i = 0; i < intSize-number2.intSize; ++i) {
+                    number2.integer="0"+number2.integer;
+                }
+
+            }
+            else if(number2.intSize>intSize){
+                for (int i = 0; i < number2.intSize-intSize; ++i) {
+                    integer="0"+integer;
+                }
+            }
+
+            cout<<true;
+        }
+
+        if(fractionSize!=number2.fractionSize){
+            if(fractionSize>number2.fractionSize){
+                for (int i = 0; i < fractionSize-number2.fractionSize; ++i) {
+                    number2.fraction=number2.fraction+"0";
+                }
+            }
+            else if (fractionSize<number2.fractionSize){
+                for (int i = 0; i < number2.fractionSize-fractionSize; ++i) {
+                   fraction =fraction+"0";
+                }
+            }
+         cout<<false;
         }
 
 
     }
+
+    bool operator==(BigReal oo2){
+        if(intSize==oo2.intSize&&fractionSize==oo2.fractionSize){
+            if(integer==oo2.integer){
+                if(fraction==oo2.fraction){
+                    return 1;
+                }
+                else{
+                    return 0;
+                }
+            }
+            else{
+                return 0;
+            }
+        }
+
+    }
+//
+//
+//    BigReal operator<(BigReal ){
+//
+//
+//
+//
+//
+//
+//
+//    }
+//
+//
+//
+//
+//
+
+
+
+
+
+
+
     void printf(){
-        cout<<integer<<" "<<fraction;
-//        cout<<BigNumber;
+        cout<<BigNumber<<"-->";
+        cout<<integer<<" "<<fraction<<" ";
+        cout<<"\n";
+
+
     }
 
 
