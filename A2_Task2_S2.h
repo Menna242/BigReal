38ad32fb2d54c289e0f09829e3ca8efbc615cbc5
Nada Adel <120988662+nadaadel50@users.noreply.github.com>
2023-11-04 02:04:19 +0200

    Update A2_Task2_S2.h

--- A2_Task2_S2.h
+++ A2_Task2_S2.h
@@ -1,8 +1,9 @@
 class BigReal{
 private:
     string BigNumber,integer,fraction;
-   long long intSize,fractionSize ;
+    long long intSize,fractionSize ;
     char sign;
+    bool firstZero;
 public:
 
     BigReal();
@@ -11,9 +12,11 @@ public:
     bool operator==(BigReal oo2);
 
 
-   int operator<(BigReal oo2 );
+    int operator<(BigReal oo2 );
 
     void printf();
+    void add(BigReal& other);
 
 
 };
+
